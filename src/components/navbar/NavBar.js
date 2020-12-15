a32f18c5684f2183dc2bd60efece862b35bab9e9
Aneudy Adames <a4aneudy@gmail.com>
2020-12-15 01:34:45 -0500

    added effect to the navigation

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,28 +1,40 @@
-import React from 'react';
+/* eslint-disable no-return-assign */
+import React, { useRef, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 // eslint-disable-next-line import/no-unresolved
 import './NavBar.css';
 
-const NavBar = () => (
-  <header>
-    <nav>
-      <h1 className="logo">WATCHED</h1>
-      <ul>
-        <li>
-          <NavLink to="/login">Log in</NavLink>
-        </li>
-        <li>
-          <NavLink to="/register">Register</NavLink>
-        </li>
-        <li>
-          <NavLink to="/auth/dashboard">Dashboard</NavLink>
-        </li>
-        <li>
-          <NavLink to="/">Log Out</NavLink>
-        </li>
-      </ul>
-    </nav>
-  </header>
-);
+const NavBar = () => {
+  const headerRef = useRef(null);
+  const [scroll, setScroll] = useState(false);
+
+  const changeBackground = () => {
+    setScroll(window.scrollY >= 20);
+  };
+
+  window.addEventListener('scroll', changeBackground);
+
+  return (
+    <header ref={headerRef} className={scroll ? 'light' : null}>
+      <nav>
+        <h1 className="logo">WATCHED</h1>
+        <ul>
+          <li>
+            <NavLink to="/login">Log in</NavLink>
+          </li>
+          <li>
+            <NavLink to="/register">Register</NavLink>
+          </li>
+          <li>
+            <NavLink to="/auth/dashboard">Dashboard</NavLink>
+          </li>
+          <li>
+            <NavLink to="/">Log Out</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export default NavBar;
